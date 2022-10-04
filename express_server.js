528e56ba6d7333eb3bdadea4b60f35a0d6777cd6
lyjeileen <lyjeileen@gmail.com>
2022-10-04 05:58:06 +0000

    Add URL submission form and add POST route

--- express_server.js
+++ express_server.js
@@ -4,7 +4,19 @@ var morgan = require("morgan");
 const PORT = 8080; // default port 8080
 
 app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+
+let shortURL = function generateRandomString() {
+  const alphanumericals =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
+  for (let i = 0; i < 6; i++) {
+    result +=
+      alphanumericals[Math.floor(Math.random() * alphanumericals.length)];
+  }
+  return result;
+};
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
@@ -14,10 +26,21 @@ const urlDatabase = {
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
+
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send(shortURL()); // Respond with 'Ok' (we will replace this)
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = {
     id: req.params.id,
