3d49f11424da07075413c120dc58de22d67af58f
lyjeileen <lyjeileen@gmail.com>
2022-10-07 00:59:49 +0000

    Add a test for urlsForUser

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -1,6 +1,6 @@
 const { assert } = require("chai");
 
-const { getUserByEmail } = require("../helpers.js");
+const { getUserByEmail, urlsForUser } = require("../helpers.js");
 
 const testUsers = {
   userRandomID: {
@@ -8,9 +8,9 @@ const testUsers = {
     email: "user@example.com",
     password: "purple-monkey-dinosaur",
   },
-  user2RandomID: {
-    id: "user2RandomID",
-    email: "user2@example.com",
+  aJ48lW: {
+    id: "aJ48lW",
+    email: "test@test.com",
     password: "dishwasher-funk",
   },
 };
@@ -25,5 +25,31 @@ describe("getUserByEmail", function () {
     const user = getUserByEmail("wrong@example.com", testUsers);
     const expectedUserID = undefined;
     assert.strictEqual(user, expectedUserID);
+  });
+});
+
+const urlDatabase = {
+  b6UTxQ: {
+    longURL: "https://www.tsn.ca",
+    userID: "userRandomID",
+  },
+  i3BoGr: {
+    longURL: "https://www.google.ca",
+    userID: "aJ48lW",
+  },
+};
+
+describe("urlsForUser", function () {
+  it("should return an object containing the urls for a specific user id. The keys are short URLs and values are long URLs", function () {
+    const validURLs = urlsForUser("aJ48lW", urlDatabase);
+    const expectedURLs = {
+      i3BoGr: "https://www.google.ca",
+    };
+    assert.deepEqual(validURLs, expectedURLs);
+  });
+  it("should return {} if database does not contain the urls for a specific user id. The keys are short URLs and values are long URLs", function () {
+    const validURLs = urlsForUser("wrong", urlDatabase);
+    const expectedURLs = {};
+    assert.deepEqual(validURLs, expectedURLs);
   });
 });
